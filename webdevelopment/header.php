2ada27ac7466626abc6aa674900e3ee827076fcd
almayers <amy.lynne.florence@gmail.com>
2026-05-03 15:27:53 -0700

    Update header.php

--- webdevelopment/header.php
+++ webdevelopment/header.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About</title>
+
+<link rel="shortcut icon" href="images/math.ico">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="nav.css">
+</head>
+
+<body>
+
 <header>
 <img src="images/MayersLogo.png">
 
@@ -5,11 +19,11 @@
 
 <nav>
 <ul>
-<li><a href="index.html">Home</a></li>
-<li><a href="about.html">About</a></li>
-<li><a href="mathcontent.html">Math Content</a></li>
-<li><a href="reflection.html">Reflection</a></li>
-<li><a href="contact.html">Contact</a></li>
+<li><a href="index.php">Home</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="mathcontent.php">Math Content</a></li>
+<li><a href="reflection.php">Reflection</a></li>
+<li><a href="contact.php">Contact</a></li>
 </ul>
 </nav>
 </header>
